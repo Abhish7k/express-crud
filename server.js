1286ad2d2c6b2ba29a1d1a1b76548302f202c63f
Abhishek <abhishekbhosale.dev@gmail.com>
2023-12-16 15:17:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,94 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import Product from "./models/productModej.js";
-
-// const app = express();
-// app.use(express.json());
-
-// const uri =
-//   "mongodb+srv://admin:admin@cluster0.cabehnw.mongodb.net/?retryWrites=true&w=majority";
-
-// // routes
-// app.get("/products", async (req, res) => {
-//   try {
-//     const products = await Product.find({});
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.get("/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findById(id);
-//     res.status(200).json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.post("/products", async (req, res) => {
-//   try {
-//     const product = await Product.create(req.body);
-//     res.status(200).json(product);
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ message: error.error });
-//   }
-// });
-
-// app.put("/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findByIdAndUpdate(id, req.body);
-
-//     if (!product) {
-//       return res
-//         .status(404)
-//         .json({ message: `cannot find any product with id ${id}` });
-//     }
-
-//     const updatedProduct = await Product.findById(id);
-
-//     res.status(200).json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.delete("/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-
-//     const product = await Product.findByIdAndDelete(id);
-
-//     if (!product) {
-//       return res
-//         .status(404)
-//         .json({ message: `Cannot find product with id ${id}` });
-//     }
-//     res.status(200).json({ message: `Product with id ${id} deleted` });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log("Server started");
-// });
-
-// mongoose.set("strictQuery");
-// mongoose
-//   .connect(uri)
-//   .then(() => {
-//     console.log("Connected to Mongodb");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// importing
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -123,7 +32,6 @@ app.get("/products/:id", async (req, res) => {
   }
 });
 
-// create
 app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -133,7 +41,6 @@ app.post("/products", async (req, res) => {
   }
 });
 
-// update
 app.put("/products/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -154,7 +61,6 @@ app.put("/products/:id", async (req, res) => {
   }
 });
 
-// delete
 app.delete("/products/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -175,12 +81,10 @@ app.delete("/products/:id", async (req, res) => {
   }
 });
 
-// listen
 app.listen(PORT, () => {
   console.log(`Server listening at Port: ${PORT}`);
 });
 
-// mongo connect
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
